f1125ebdd9ac568ac116f888374334bfaf3c3ddb
Otavio Rodolfo Piske <opiske@redhat.com>
2018-07-22 14:56:39 +0200

    FIX: incorrect log directory locations

--- extra/docker-compose/maestro/workers/sender/maestro-container-wrapper.sh
+++ extra/docker-compose/maestro/workers/sender/maestro-container-wrapper.sh
@@ -2,6 +2,6 @@
 echo "Synchronizing time"
 ntpd -d -q -n -p pool.ntp.org
 MAESTRO_BROKER=${MAESTRO_BROKER:-mqtt://broker:1883}
-MAESTRO_LOG_DIR=${MAESTRO_LOG_DIR:-/maestro/receiver/logs}
+MAESTRO_LOG_DIR=${MAESTRO_LOG_DIR:-/maestro/sender/logs}
 
 ${MAESTRO_APP_ROOT}/maestro-worker/bin/maestro-worker -m ${MAESTRO_BROKER} -w org.maestro.worker.jms.JMSSenderWorker -l ${MAESTRO_LOG_DIR} -r sender
